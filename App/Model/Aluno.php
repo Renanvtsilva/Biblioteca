e2f9085bb7fa1068ae414a187b311c4bfd94d526
Renanvtsilva <renan.viana@futcorp.com.br>
2025-01-08 17:35:23 -0300

    Alteração em Aluno.php

--- App/Model/Aluno.php
+++ App/Model/Aluno.php
@@ -1,6 +1,30 @@
 <?php
 
+namespace App\Model;
+
 class Aluno
 {
+    public $id, $nome, $ra, $curso;
+
+    function save() : Aluno
+    {
+        // Salvar aluno no banco de dados
+        return new Aluno();
+    }
+
+    function getById(int $id) : ?Aluno
+    {
+        return new Aluno();
+    }
+
+    function getAllRows() : array
+    {
+        return [];
+    }
+
+    function delete(int $id) : bool
+    {
+        return false;
+    }
     
 }
